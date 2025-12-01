6ff3b8ea0ee926710a8c77019440a6034b414ce8
Alexey Nehaenko <Nehaenkog23@gmail.com>
2025-12-01 19:45:00 +0300

    Add domain logic: jwt, data_models, password util

---
+++ services/auth_service/internal/domain/model/credentials.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"github.com/alexey-dobry/fileshare/pkg/validator"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Credentials struct {
+	gorm.Model
+	UUID         uuid.UUID `gorm:"not null" validate:"required"`
+	Email        string    `gorm:"not null,uniqueIndex" validate:"required"`
+	PasswordHash string    `gorm:"not null" validate:"required"`
+}
+
+func (u *Credentials) Validate() error {
+	return validator.V.Struct(u)
+}
